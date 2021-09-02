10aa986a05b976b8a899e5d7aaefe3f891fdedb7
Soner Koçak <sonerkocak@gmail.com>
2021-09-02 13:12:14 +0300

    edited samples

---
+++ f02.js
@@ -0,0 +1,49 @@
+const { readFileSync } = require('fs')
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const randomWords = require('random-words');
+const {buildSchema} = require("graphql");
+
+let id = 0;
+const POSTS = [];
+
+const schema = buildSchema(readFileSync('./f02.graphql').toString('utf-8'));
+
+const root = {
+    posts: () => POSTS,
+    addPost: ({ input: { text, tags } }) => {
+        POSTS.push({id: `${++id}`, text, tags, comments: []});
+        console.log('POSTS', POSTS);
+        return 'OK';
+    },
+    addComment: ({ id, input: { text } }) => {
+        const post = POSTS.find( val => val.id === id);
+        post.comments.push({id: `${++id}`, text});
+        console.log('POSTS', POSTS);
+        return 'OK';
+    },
+
+};
+
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+/*
+mutation postEkle {
+  addPost(input: {text: "Post 01", tags: ["a", "b"]})
+  addComment(id: 1, input: {text: "Comment 01"})
+}
+
+query sorgula {
+  posts {
+    id, text, tags, comments {id, text}
+  }
+}
+ */
